96a99f1bf521286257635c69171f1ab72017eb3a
Dabere Tech Ai <tustechworld@gmail.com>
2026-07-10 09:22:51 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+// Database configuration
+return [
+    'host' => 'localhost',
+    'database' => 'login_system',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8mb4'
+];
